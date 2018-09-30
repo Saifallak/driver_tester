0083a958484cd1af8b4dcf22bca95ae8ac764d85
Saifallak <saifallak@yahoo.com>
2018-09-30 21:11:11 +0200

    4th Commit
    Signed-off-by: Saifallak <Saifallak@yahoo.com>

--- test_driver/JsonParseTest_test.dart
+++ test_driver/JsonParseTest_test.dart
@@ -1,15 +1,27 @@
+import 'dart:async';
+import 'dart:io';
+
 import 'package:flutter_driver/flutter_driver.dart';
 import 'package:test/test.dart';
 
 void main() {
   group("Json Show Testing", () {
-    FlutterDriver driver;
+    FlutterDriver driver; // Flutter Driver Var
+    StreamSubscription<HttpRequest> httpStream; // Http Server
+
     setUpAll(() async {
       driver = await FlutterDriver.connect();
+      HttpServer.bind("localhost", 8899).then((server) {
+        httpStream = server.listen((HttpRequest request) {
+          request.response.write('{ "greeting": "bonjour" }');
+          request.response.close();
+        });
+      });
     });
     tearDownAll(() async {
       if (driver != null) {
-        driver.close();
+        driver.close(); // Stop Driver
+        httpStream.cancel(); // Stop Http Server
       }
     });
     test("Json Showed Correctly?!", () async {
