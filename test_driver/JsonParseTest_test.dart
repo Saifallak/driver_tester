8f6bd576b1d80bb26932fd826d6b4ea4b8f9733f
Saifallak <saifallak@yahoo.com>
2018-10-01 23:19:44 +0200

    6th Commit
    Signed-off-by: Saifallak <Saifallak@yahoo.com>

--- test_driver/JsonParseTest_test.dart
+++ test_driver/JsonParseTest_test.dart
@@ -17,38 +17,23 @@ void main() {
 
     setUpAll(() async {
       driver = await FlutterDriver.connect();
-      HttpServer.bind("localhost", 8899).then((server) {
-        httpStream = server.listen((HttpRequest request) {
-          request.response.write('{ "greeting": "bonjour" }');
-          request.response.close();
-        });
-      });
+//      HttpServer.bind("localhost", 8899).then((server) {
+//        httpStream = server.listen((HttpRequest request) {
+//          request.response.write('{ "greeting": "bonjour" }');
+//          request.response.close();
+//        });
+//      });
     });
     tearDownAll(() async {
       if (driver != null) {
         driver.close(); // Stop Driver
-        httpStream.cancel(); // Stop Http Server
+        httpStream?.cancel(); // Stop Http Server
       }
     });
     test("Json Showed Correctly?!", () async {
       // this expect return True
-      expect(
-          await driver.getRenderTree().then((x) {
-            return x.tree.contains("greeting");
-          }),
-          true);
-
-//      try {
-//        await driver.waitForAbsent(
-//            await driver.getRenderTree().then((x) {
-//              print(x.tree.contains("greeting"));
-//            }),
-//            timeout: const Duration(seconds: 5));
-//        fail('expected DriverError');
-//      } on DriverError catch (error) {
-//        expect(
-//            error.message, contains('Timeout while executing waitForAbsent'));
-//      }
+      SerializableFinder finder = find.text('howdie');
+      driver.getText(finder, timeout: Duration(seconds: 10));
     });
   });
 }
